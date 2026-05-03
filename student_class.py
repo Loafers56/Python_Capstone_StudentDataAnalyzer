9e406c9f6ec55978a3d5b0c57bfc50912ee34e29
Loafers56 <ns2046964@gmail.com>
2026-05-03 10:06:40 -0700

    Update student_class.py

--- student_class.py
+++ student_class.py
@@ -45,3 +45,16 @@ class Student(StudentBase):
                 return "C"
             else:
                 return "F"
+    
+    # Shows all of a students data
+    def show_data(self):
+        data = (
+        f"Student: {self.name}\n"
+        f"Grade level: {self.grade_level}\n"
+        f"Grades: {self.grades}\n"
+        f"Average: {self.average()}\n"
+        f"Highest grade: {self.highest_score()}\n"
+        f"Lowest grade: {self.lowest_score()}\n"
+        f"Letter grades: {self.number_to_letter()}\n"
+        )
+        return data
